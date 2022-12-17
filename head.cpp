9a02068260a924ae0ffad41b323d2914cbb50aba
single <72294271+Rw-chen@users.noreply.github.com>
2022-12-17 17:33:23 +0800

    Add files via upload

---
+++ head.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file      head.cpp
+ * @author    rw-chen (@foxmail.com)
+ * @brief     template
+ * @version   0.1
+ * @date 	  2022-11-26 22:05:16
+ * @copyright Copyright (c) 2022
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define INF 0x3f3f3f3f
+#define SZ(x) ((int)(x.size()))
+#define all(x) x.begin(), x.end()
+#define ios() cin.tie(nullptr)->sync_with_stdio(false)
+using vi = vector<int>;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+const ll mod = 1e9 + 7;
+const double eps = 1e-8;
+/************************<head>*************************/
+
+/*********************<begin code>**********************/
+
+void solve() {
+
+}
+
+int main() {
+	#ifdef LOCAL
+	freopen("in.txt", "r", stdin);
+	#endif 
+	ios();
+	solve();
+	return 0;
+}
